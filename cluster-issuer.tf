5541e522fb1eaf74658579f0f0285d16c51bd09a
f3lin <alex.nguepi@yahoo.fr>
2024-08-06 13:47:58 +0200

    remove cluster issuer

--- cluster-issuer.tf
+++ cluster-issuer.tf
@@ -1,30 +1,30 @@
-resource "kubernetes_manifest" "cluster_issuer" {
-  manifest = {
-    "apiVersion" = "cert-manager.io/v1"
-    "kind"       = "Issuer"
-    "metadata" = {
-      "name" = "http-01-${var.env}"
-    }
-
-    spec = {
-      acme = {
-        email  = var.user_email
-        server = "https://acme-v02.api.letsencrypt.org/directory"
-        privateKeySecretRef = {
-          name = "http-01-${var.env}-cluster-issuer"
-        }
-        solvers = [
-          {
-            http01 = {
-              ingress = {
-                ingressClassName = "external-nginx"
-              }
-            }
-          }
-        ]
-      }
-    }
-  }
-
-  depends_on = [helm_release.cert-manager]
-}
+# resource "kubernetes_manifest" "cluster_issuer" {
+#   manifest = {
+#     "apiVersion" = "cert-manager.io/v1"
+#     "kind"       = "Issuer"
+#     "metadata" = {
+#       "name" = "http-01-${var.env}"
+#     }
+#
+#     spec = {
+#       acme = {
+#         email  = var.user_email
+#         server = "https://acme-v02.api.letsencrypt.org/directory"
+#         privateKeySecretRef = {
+#           name = "http-01-${var.env}-cluster-issuer"
+#         }
+#         solvers = [
+#           {
+#             http01 = {
+#               ingress = {
+#                 ingressClassName = "external-nginx"
+#               }
+#             }
+#           }
+#         ]
+#       }
+#     }
+#   }
+#
+#   depends_on = [helm_release.cert-manager]
+# }
