4eee0330a35a1670966ee8deb38536efb7c32e7b
f3lin <alex.nguepi@yahoo.fr>
2024-08-06 22:57:02 +0200

    wip: deployment with ecr images

--- providers.tf
+++ providers.tf
@@ -11,8 +11,9 @@ terraform {
 
 provider "aws" {
   region = "eu-west-3"
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "student16_mai24_bootcamp_devops_sock-shop"
+  # for local dev
+#   shared_credentials_files = ["~/.aws/credentials"]
+#   profile                  = "student16_mai24_bootcamp_devops_sock-shop"
 }
 
 
