d706e6737ef1e2d056647cb614b68f5d6d4c40b8
f3lin <alex.nguepi@yahoo.fr>
2024-08-06 18:54:27 +0200

    use image from ecr

--- helm-deployment.tf
+++ helm-deployment.tf
@@ -1,3 +1,11 @@
+data "aws_ecr_repository" "service_catalogue" {
+  name = "catalogue"
+}
+
+data "aws_ecr_repository" "service_front_end" {
+  name = "front-end"
+}
+
 resource "helm_release" "sock-shop" {
   name             = "microservices-app-master-release"
   chart            = "sock-shop"
@@ -8,4 +16,24 @@ resource "helm_release" "sock-shop" {
   values = [
     file(var.values_yaml_path)
   ]
+
+  set {
+    name  = "frontEnd.image.repository"
+    value = data.aws_ecr_repository.service_front_end.repository_url
+  }
+
+  set {
+    name  = "frontEnd.image.tag"
+    value = data.aws_ecr_repository.service_front_end.tags[0]
+  }
+
+  set {
+    name  = "catalogue.image.repository"
+    value = data.aws_ecr_repository.service_catalogue.repository_url
+  }
+
+  set {
+    name  = "catalogue.image.tag"
+    value = data.aws_ecr_repository.service_catalogue.tags[0]
+  }
 }
