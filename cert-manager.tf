ef1b62e055eff6e6cefc5d297fabd5296899450a
f3lin <alex.nguepi@yahoo.fr>
2024-08-07 22:01:41 +0200

    deploy shop-sock

--- cert-manager.tf
+++ cert-manager.tf
@@ -14,3 +14,33 @@ resource "helm_release" "cert-manager" {
 
   depends_on = [helm_release.external-nginx]
 }
+
+resource "kubernetes_manifest" "cluster_issuer" {
+  manifest = yamlencode({
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+    metadata = {
+      name = "http-01-${var.env}"
+    }
+    spec = {
+      acme = {
+        email = "alex@jmetio.de"
+        server = "https://acme-v02.api.letsencrypt.org/directory"
+        privateKeySecretRef = {
+          name = "http-01-${var.env}-cluster-issuer"
+        }
+        solvers = [
+          {
+            http01 = {
+              ingress = {
+                ingressClassName = "external-nginx"
+              }
+            }
+          }
+        ]
+      }
+    }
+  })
+
+  depends_on = [helm_release.cert-manager]
+}
