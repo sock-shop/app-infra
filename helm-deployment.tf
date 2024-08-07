586ce1a94ec928ab7a3736e2d31d1e36e722bdd0
f3lin <alex.nguepi@yahoo.fr>
2024-08-07 23:03:09 +0200

    deploy shop-sock

--- helm-deployment.tf
+++ helm-deployment.tf
@@ -1,17 +1,17 @@
-# resource "helm_release" "sock-shop" {
-#   name             = "sock-shop-release"
-#   chart            = "sock-shop"
-#   create_namespace = true
-#   namespace        = var.env
-#   wait             = true
-#
-#   values = [
-#     file(var.values_yaml_path)
-#   ]
-#
-#   depends_on = [
-#     helm_release.external-nginx,
-#     helm_release.cert-manager,
-#     #     kubernetes_manifest.cluster_issuer
-#   ]
-# }
+resource "helm_release" "sock-shop" {
+  name             = "sock-shop-release"
+  chart            = "sock-shop"
+  create_namespace = true
+  namespace        = var.env
+  wait             = true
+
+  values = [
+    file(var.values_yaml_path)
+  ]
+
+  depends_on = [
+    helm_release.external-nginx,
+    helm_release.cert-manager,
+    #     kubernetes_manifest.cluster_issuer
+  ]
+}
