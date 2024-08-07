d986bd27facf66babd899337ed9bf739ab8630d0
f3lin <alex.nguepi@yahoo.fr>
2024-08-07 22:04:01 +0200

    clean code

--- helm-deployment.tf
+++ helm-deployment.tf
@@ -8,4 +8,10 @@ resource "helm_release" "sock-shop" {
   values = [
     file(var.values_yaml_path)
   ]
+
+  depends_on = [
+    helm_release.external-nginx,
+    helm_release.cert-manager,
+    kubernetes_manifest.cluster_issuer
+  ]
 }
