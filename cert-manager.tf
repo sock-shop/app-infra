a4eb629c9f7bd772822041d29c328d5bce49a6f8
f3lin <alex.nguepi@yahoo.fr>
2024-08-08 10:08:49 +0200

    deploy shop-sock

--- cert-manager.tf
+++ cert-manager.tf
@@ -1,11 +1,11 @@
 resource "helm_release" "cert-manager" {
-  name = "cert-manager"
-
+  name             = "cert-manager"
   repository       = "https://charts.jetstack.io"
   chart            = "cert-manager"
   namespace        = "cert-manager"
   create_namespace = true
   version          = "v1.14.5"
+  wait             = true
 
   set {
     name  = "installCRDs"
@@ -14,13 +14,3 @@ resource "helm_release" "cert-manager" {
 
   depends_on = [helm_release.external-nginx]
 }
-
-# resource "kubernetes_manifest" "cluster_issuer" {
-#   manifest = yamldecode(templatefile("${path.module}/cluster_issuer.tpl", {
-#     env                   = var.env,
-#     email                 = "alex@jmetio.de",
-#     private_key_secret    = "http-01-${var.env}-cluster-issuer",
-#     ingress_class_name    = "external-nginx"
-#   }))
-#   depends_on = [helm_release.cert-manager]
-# }
--- nginx-ingress.tf
+++ nginx-ingress.tf
@@ -1,11 +1,11 @@
 resource "helm_release" "external-nginx" {
-  name = "external"
-
+  name             = "external"
   repository       = "https://kubernetes.github.io/ingress-nginx"
   chart            = "ingress-nginx"
   namespace        = "ingress"
   create_namespace = true
   version          = "4.10.1"
+  wait             = true
 
   values = [file("nginx-ingress.yaml")]
 }
